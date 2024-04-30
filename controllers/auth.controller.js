8fd4aff2d4c0fdc96862ca6f987f3f6fe90c5903
Tomsss <tomasito21010@gmail.com>
2024-04-29 21:51:17 -0300

    ????

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,10 +1,24 @@
-export const register =  (req,res) =>{
+import { User } from "../models/user.js";
 
-  console.log(req.body);
-  res.json({ok: "you are registered"});
+export const register = async (req,res) =>{
+  const {email, password} = req.body
+  try {
+    let user = new User({email, password})
+    await user.save()
+
+    //jwt token
+
+    return res.json({});
+  } catch (error) {
+    console.log("code:", error.code);
+    //default from mongoose
+    if(error.code === 11000){
+      return res.status(400).json({error: 'User already exist'})
+    }
+  }
 };
 
-export const login = (req,res) =>{
+export const login = async (req,res) =>{
   res.json({ok: "login"});
 };
 
--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
-import {Schema, model} from "mongoose";
+import mongoose from "mongoose";
+import bcryptjs from 'bcryptjs';
 
-const userSchema = new Schema({
+const userSchema = new mongoose.Schema({
   email:{
     type: String,
     require: true,
@@ -8,7 +9,6 @@ const userSchema = new Schema({
     unique: true,
     lowercase: true,
     inde: { unique: true },
-
   },
   password:{
     type: String,
@@ -16,5 +16,20 @@ const userSchema = new Schema({
 
   }
 })
+userSchema.pre("save", async function(next){
+  const user = this;
 
-export const User = model('user', userSchema)
+if(!user.isModified("password")) return next()
+
+ try{
+  const salt = await bcryptjs.genSalt(10)
+  user.password = await bcryptjs.hash(user.password, salt)
+next() 
+}catch (error) {
+    console.log(error)
+    throw new Error("Hash this wrong")
+ }
+})
+
+
+export const User = mongoose.model('User', userSchema)
